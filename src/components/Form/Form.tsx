2d1373406e00b87cbd7438d433cc3c37bb3b6229
Мария <maria@MacBook-Air-Maria.local>
2025-02-10 23:03:54 +0400

    feat: add form component

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Form.css";
+import {Select} from "antd";
+import {searchOptions} from "../../data/searchOptions";
+
+function Form() {
+    return(
+        <section className="form-section">
+            <h1 className="form-section__title">Форма размещения</h1>
+            <form className="form">
+                <input className="form__input" placeholder="Название авто/услуги/недвижимости"
+                       type="text" required maxLength={30} minLength={5}/>
+                <textarea className="form__input" placeholder="Описание авто/услуги/недвижимости"
+                          required maxLength={100} minLength={5} />
+                <input className="form__input" placeholder="Локация авто/услуги/недвижимости"
+                       type="text" required maxLength={30} minLength={5} />
+                <input className="form__input" placeholder="Фото авто/услуги/недвижимости"
+                       type="url" required />
+                <Select placeholder="Выберете категорию" style={{width: "100%"}} options={searchOptions} />
+
+                <button className="form__button">Отправить</button>
+            </form>
+        </section>
+    )
+}
+
+export default Form;
--- src/pages/MainPage/MainPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function MainPage() {
-    return (
-        <h1>Main page</h1>
-    )
-}
-
-export default MainPage;
