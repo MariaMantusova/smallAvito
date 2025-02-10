10db4438728c1c8b474016fa26272fb6d91349b7
Мария <maria@MacBook-Air-Maria.local>
2025-02-10 18:36:41 +0400

    feat: add footer component

--- src/pages/ListPage/ListPage.tsx
+++ src/pages/ListPage/ListPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import List from "../../components/List/List";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 function ListPage() {
     return (
         <>
             <Header/>
             <List/>
+            <Footer/>
         </>
     )
 }
