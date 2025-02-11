607ca24577b14c736667480c3450ade84e0dea42
Мария <maria@MacBook-Air-Maria.local>
2025-02-11 15:02:00 +0400

    fix: class fixed

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -19,9 +19,7 @@ function Form() {
                 <input className="form__input" placeholder="Название авто/услуги/недвижимости"
                        type="text" required onBlur={title.onBlur} onChange={title.onChange}/>
                 <p className={`form__error ${((title.isEmpty && title.isDirty) ||
-                    !(title.isEmpty && title.isDirty && title.stringError) ||
-                    !(title.isEmpty && title.isDirty && title.minLengthError) ||
-                    !(title.isEmpty && title.isDirty && title.maxLengthError)) && "form__error_visible"}`}>
+                    (!title.isEmpty && title.isDirty && (title.stringError || title.minLengthError || title.maxLengthError))) && "form__error_visible"}`}>
                     {(title.isEmpty && title.isDirty) && "Поле не может быть пустым"}
                     {(!title.isEmpty && title.isDirty && title.stringError) && "Поле не может содержать данный символ"}
                     {(!title.isEmpty && title.isDirty && title.minLengthError) && "Слишком короткое название"}
@@ -30,9 +28,7 @@ function Form() {
                 <textarea className="form__input" placeholder="Описание авто/услуги/недвижимости"
                           required onBlur={description.onBlur} onChange={description.onChange}/>
                 <p className={`form__error ${((description.isEmpty && description.isDirty) ||
-                    !(description.isEmpty && description.isDirty && description.stringError) ||
-                    !(description.isEmpty && description.isDirty && description.minLengthError) ||
-                    !(description.isEmpty && description.isDirty && description.maxLengthError)) && "form__error_visible"}`}>
+                    (!description.isEmpty && description.isDirty && (description.stringError || description.minLengthError || description.maxLengthError))) && "form__error_visible"}`}>
                     {(description.isEmpty && description.isDirty) && "Поле не может быть пустым"}
                     {(!description.isEmpty && description.isDirty && description.stringError) && " Поле не может содержать данный символ"}
                     {(!description.isEmpty && description.isDirty && description.minLengthError) && " Слишком короткое описание"}
@@ -41,10 +37,8 @@ function Form() {
                 <input className="form__input" placeholder="Локация авто/услуги/недвижимости"
                        type="text" required maxLength={30} minLength={5} onBlur={location.onBlur}
                        onChange={location.onChange}/>
-                <p className={`form__error ${((location.isEmpty && location.isDirty) ||
-                    !(location.isEmpty && location.isDirty && location.stringError) ||
-                    !(location.isEmpty && location.isDirty && location.minLengthError) ||
-                    !(location.isEmpty && location.isDirty && location.maxLengthError)) && "form__error_visible"}`}>
+                <p className={`form__error ${((location.isEmpty && location.isDirty) || 
+                    (!location.isEmpty && location.isDirty && (location.stringError || location.minLengthError || location.maxLengthError))) && "form__error_visible"}`}>
                     {(location.isEmpty && location.isDirty) && "Поле не может быть пустым"}
                     {(!location.isEmpty && location.isDirty && location.stringError) && " Поле не может содержать данный символ"}
                     {(!location.isEmpty && location.isDirty && location.minLengthError) && " Слишком короткое описание локации"}
@@ -52,10 +46,7 @@ function Form() {
                 </p>
                 <input className="form__input" placeholder="Фото авто/услуги/недвижимости"
                        type="url" required onBlur={photo.onBlur} onChange={photo.onChange}/>
-                <p className={`form__error ${(
-                    !(photo.isEmpty && photo.isDirty && photo.urlError) ||
-                    !(photo.isEmpty && photo.isDirty && photo.minLengthError) ||
-                    !(photo.isEmpty && photo.isDirty && photo.maxLengthError)) && "form__error_visible"}`}>
+                <p className={`form__error ${(!photo.isEmpty && photo.isDirty && (photo.urlError || photo.minLengthError || photo.maxLengthError)) && "form__error_visible"}`}>
                     {(!photo.isEmpty && photo.isDirty && photo.urlError) && " Данная ссылка не действительна"}
                     {(!photo.isEmpty && photo.isDirty && photo.minLengthError) && " Слишком короткая ссылка"}
                     {(!photo.isEmpty && photo.isDirty && photo.maxLengthError) && " Слишком длинная ссылка"}
