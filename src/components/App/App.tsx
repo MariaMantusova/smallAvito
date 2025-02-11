f5b96eb52f3b30f600a7a687e0b6d0e9118181d9
Мария <maria@MacBook-Air-Maria.local>
2025-02-11 12:09:38 +0400

    feat: add not found page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router";
 import ItemPage from "../../pages/ItemPage/ItemPage";
 import ListPage from "../../pages/ListPage/ListPage";
 import FormPage from "../../pages/FormPage/FormPage";
+import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Route path="/form" element={<FormPage />}></Route>
             <Route path="/list" element={<ListPage />}></Route>
             <Route path="/item/:id" element={<ItemPage />}></Route>
+            <Route path="*" element={<NotFoundPage />}></Route>
         </Routes>
     )
 }
